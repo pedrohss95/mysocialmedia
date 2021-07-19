073750b4b582e08f1770aa19ccc5d4c00ae92c11
phsoares <phsoares@github.ibm.com>
2021-07-19 19:09:53 -0300

    creating new project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+  
+`;
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+
+  @media(min-width: 860px){
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea:'profileArea' }}>
+        <Box>
+          Home
+          <img src="https://github.com/pedrohss95.png" />
+        </Box>
+      </div>
+      <div style={{ gridArea:'welcomeArea' }}>
+        <Box>
+          Welcome
+        </Box>
+      </div>
+      <div style={{ gridArea:'profileRelationsArea' }}>
+        <Box>
+          Good evening
+        </Box>
+        <Box>
+          Good night
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
